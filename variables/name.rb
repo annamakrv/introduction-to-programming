35e0ad0fff70fc84b50561fd0f9dc65f6cdcd38e
Anna Makarova <annamakrv@gmail.com>
2017-05-06 18:14:15 -0400

    commit variables

---
+++ variables/name.rb
@@ -0,0 +1,17 @@
+# name.rb first part
+puts "What is your name?"
+name = gets.chomp
+puts "Hello " + name + "!"
+
+# name.rb second part
+
+10.times do
+  puts name
+end
+
+# name.rb third part
+puts "What is your first name?"
+first_name = gets.chomp
+puts "What is your last name?"
+last_name = gets.chomp
+puts "Hello " + first_name + " " + last_name + "!"
